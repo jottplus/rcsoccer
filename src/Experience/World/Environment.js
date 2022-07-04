666987a92ed5008c5c6a3877900efda893a934bd
jottplus <jfisch.r92@gmail.com>
2022-07-04 20:26:44 +0200

    removed sun, added spots, added ambient light

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -6,16 +6,33 @@ export default class Environment {
         this.experience = new Experience()
         this.scene = this.experience.scene
 
-        this.setSunlight()
+        this.data = [
+            [-5, 10, -5],
+            [-5, 10, 5],
+            [5, 10, -5],
+            [5, 10, 5]
+        ]
+
+        this.setDirectionalLights()
+        this.setAmbientLight()
     }
 
-    setSunlight() {
-        this.sunlight = new THREE.DirectionalLight('#ffffff', 4)
-        this.sunlight.castShadow = true
-        this.sunlight.shadow.camera.far = 15
-        this.sunlight.shadow.mapSize.set(1024, 1024)
-        this.sunlight.shadow.normalBias = 0.05
-        this.sunlight.position.set(3.5, 2, -1.25)
-        this.scene.add(this.sunlight)
+    setDirectionalLights() {
+        this.directionLights = []
+        for (const d of this.data) {
+            const light = new THREE.DirectionalLight(0xffffff, 0.3)
+            light.position.set(...d)
+            light.castShadow = true
+            this.scene.add(light)
+        }
+        this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.3)
+        this.directionalLight.position.set(-5, 10, -5)
+        this.directionalLight.castShadow = true
+        this.scene.add(this.directionalLight)
+    }
+
+    setAmbientLight() {
+        this.ambientLight = new THREE.AmbientLight(0x1700c2, 0.5)
+        this.scene.add(this.ambientLight)
     }
 }
